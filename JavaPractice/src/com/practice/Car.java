10d6a2348e2cdf3c141d7410d431db8be136744a
aleramesh <aleramesh@79c8fb9c-87f8-5ed7-9c19-bd964ec85716>
2010-07-03 14:51:35 +0000

    Uploading the project

---
+++ JavaPractice/src/com/practice/Car.java
@@ -0,0 +1,20 @@
+package com.practice;
+
+public class Car {
+	
+	private String brandname;
+	private int year;
+	public String getBrandname() {
+		return brandname;
+	}
+	public void setBrandname(String brandname) {
+		this.brandname = brandname;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+}
